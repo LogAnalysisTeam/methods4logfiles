baee011e93ccc29a0d3c8edd7efbc750bcd31392
Martin Korytak <cbudrud@gmail.com>
2021-01-17 12:47:27 +0100

    support for Dict in splitting utils

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from sklearn import model_selection
 from collections import defaultdict
-from typing import Dict
+from typing import Dict, Union
 
 
-def get_data_by_indices(data: defaultdict, labels: pd.DataFrame) -> Dict:
+def get_data_by_indices(data: Union[defaultdict, Dict], labels: pd.DataFrame) -> Dict:
     ret = {block_id: data[block_id] for block_id in labels['BlockId']}
     return ret
 
 
-def train_test_split(data: defaultdict, labels: pd.DataFrame, test_size: float, seed: int) -> tuple:
+def train_test_split(data: Union[defaultdict, Dict], labels: pd.DataFrame, test_size: float, seed: int) -> tuple:
     # assumes that one block is one label, otherwise it would generate more data
     train_labels, test_labels = model_selection.train_test_split(labels, stratify=labels['Label'], test_size=test_size,
                                                                  random_state=seed)

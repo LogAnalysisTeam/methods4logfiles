8cfa2dd9de0b58a18265fd82e0f89eae31c658d0
Martin Korytak <cbudrud@gmail.com>
2021-01-17 17:10:00 +0100

    prepared scripts for loading and splitting HDFS1 data set into training, testing and validation sets

--- src/data/hdfs.py
+++ src/data/hdfs.py
@@ -1,13 +1,12 @@
 import pandas as pd
+import numpy as np
 import os
 from collections import defaultdict
 import re
-from typing import Dict
+from typing import Dict, Union, Generator
+from sklearn import model_selection
 
-from utils import train_test_split
-
-DATA_DIR = '../../data/raw/HDFS1/'
-OUTPUT_DIR = '../../data/interim/HDFS1'
+SEED = 160121
 
 
 def load_labels(file_path: str) -> pd.DataFrame:
@@ -39,25 +38,60 @@ def save_logs_to_file(data: Dict, file_path: str):
             f.write(logs)
 
 
-def process_hdfs(save_to_file: bool = True, test_size: float = 0.1) -> tuple:
+def get_data_by_indices(data: Union[defaultdict, Dict], labels: pd.DataFrame) -> Dict:
+    ret = {block_id: data[block_id] for block_id in labels['BlockId']}
+    return ret
+
+
+def stratified_train_test_split(data: Union[defaultdict, Dict], labels: pd.DataFrame, test_size: float,
+                                seed: int) -> tuple:
+    # assumes that one block is one label, otherwise it would generate more data
+    train_labels, test_labels = model_selection.train_test_split(labels, stratify=labels['Label'], test_size=test_size,
+                                                                 random_state=seed)
+
+    train_data = get_data_by_indices(data, train_labels)
+    test_data = get_data_by_indices(data, test_labels)
+    return train_data, test_data, train_labels, test_labels
+
+
+def process_hdfs(data_dir: str, output_dir: str = None, save_to_file: bool = True, test_size: float = 0.1) -> tuple:
     """
     The logs are sliced into traces according to block ids. Then each trace associated with a specific block id is
     assigned a ground-truth label.
     :return:
     """
-    labels = load_labels(os.path.join(DATA_DIR, 'anomaly_label.csv'))
-    data = load_data(os.path.join(DATA_DIR, 'HDFS.log'))
+    labels = load_labels(os.path.join(data_dir, 'anomaly_label.csv'))
+    data = load_data(os.path.join(data_dir, 'HDFS.log'))
 
-    train_data, test_data, train_labels, test_labels = train_test_split(data, labels, seed=160121, test_size=test_size)
+    train_data, test_data, train_labels, test_labels = stratified_train_test_split(data, labels, seed=SEED,
+                                                                                   test_size=test_size)
 
-    if save_to_file:
-        save_logs_to_file(train_data, os.path.join(OUTPUT_DIR, 'train-data-HDFS1.log'))
-        save_logs_to_file(test_data, os.path.join(OUTPUT_DIR, 'test-data-HDFS1.log'))
-        train_labels.to_csv(os.path.join(OUTPUT_DIR, 'train-labels-HDFS1.csv'), index=False)
-        test_labels.to_csv(os.path.join(OUTPUT_DIR, 'test-labels-HDFS1.csv'), index=False)
+    if save_to_file and output_dir:
+        save_logs_to_file(train_data, os.path.join(output_dir, 'train-data-HDFS1.log'))
+        save_logs_to_file(test_data, os.path.join(output_dir, 'test-data-HDFS1.log'))
+        train_labels.to_csv(os.path.join(output_dir, 'train-labels-HDFS1.csv'), index=False)
+        test_labels.to_csv(os.path.join(output_dir, 'test-labels-HDFS1.csv'), index=False)
     return train_data, test_data, train_labels, test_labels
 
 
-if __name__ == '__main__':
-    # print(load_labels(os.path.join(DATA_DIR, 'anomaly_label.csv'))['Label'].value_counts())
-    process_hdfs()
+def get_train_val_hdfs(data: Dict, labels: pd.DataFrame, n_folds: int, test_size: float = 0.1) -> Generator:
+    if n_folds == 1:  # it isn't CV but train_test_split
+        yield stratified_train_test_split(data, labels, seed=SEED, test_size=test_size)
+    else:
+        skf = model_selection.StratifiedKFold(n_folds, shuffle=True, random_state=SEED)
+        for train_index, test_index in skf.split(np.zeros(len(labels)), labels['Label']):  # data is not important here
+            train_labels = labels.iloc[train_index]
+            test_labels = labels.iloc[test_index]
+            train_data = get_data_by_indices(data, train_labels)
+            test_data = get_data_by_indices(data, test_labels)
+            yield train_data, test_data, train_labels, test_labels
+
+
+def prepare_and_save_splits(data_dir: str, output_dir: str, n_folds: int):
+    train_data, _, train_labels, _ = process_hdfs(data_dir, output_dir)
+    splits = get_train_val_hdfs(train_data, train_labels, n_folds)
+    for idx, (train_data, test_data, train_labels, test_labels) in enumerate(splits, start=1):
+        save_logs_to_file(train_data, os.path.join(output_dir, f'train-data-HDFS1-cv-{idx}-{n_folds}.log'))
+        save_logs_to_file(test_data, os.path.join(output_dir, f'val-data-HDFS1-cv-{idx}-{n_folds}.log'))
+        train_labels.to_csv(os.path.join(output_dir, f'train-labels-HDFS1-cv-{idx}-{n_folds}.csv'), index=False)
+        test_labels.to_csv(os.path.join(output_dir, f'val-labels-HDFS1-cv-{idx}-{n_folds}.csv'), index=False)
--- src/data/prepare_hdfs.py
+++ src/data/prepare_hdfs.py
@@ -1,8 +1,24 @@
+import argparse
+
+from hdfs import prepare_and_save_splits
 
 
-
-from hdfs import process_hdfs
+def custom_int_type(x: str) -> int:
+    tmp = int(x)
+    if tmp < 1:
+        raise argparse.ArgumentTypeError('This value cannot be less than 1.')
+    return tmp
 
 
 if __name__ == '__main__':
-    train_data, _, train_labels, _ = process_hdfs()
+    parser = argparse.ArgumentParser(description='Process HDFS1 data set and split data into training, validation and '
+                                                 'testing sets.')
+    parser.add_argument('n_folds', action='store', type=custom_int_type, help='a number of cross validation splits')
+    parser.add_argument('-in', '--input', type=str, metavar='PATH/TO/FOLDER', default='../../data/raw/HDFS1/',
+                        help='a location with HDFS1 data (HDFS.log and anomaly_label.csv)')
+    parser.add_argument('-out', '--output', type=str, metavar='PATH/TO/FOLDER', default='../../data/interim/HDFS1',
+                        help='a location where all intermediate data will be saved')
+
+    args = parser.parse_args()
+
+    prepare_and_save_splits(args.input, args.output, args.n_folds)

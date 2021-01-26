215300f2ff5d461017823041e3cb04b0487d34d0
Martin Korytak <cbudrud@gmail.com>
2021-01-26 14:47:34 +0100

    rewritten structure using data object i.e., a dict with block_id as a key and logs as the values, output is saved to binary format

--- src/data/logparser.py
+++ src/data/logparser.py
@@ -1,6 +1,7 @@
-import pandas as pd
 from drain3 import TemplateMiner
-from typing import List, Dict
+from collections import defaultdict
+import pickle
+from typing import List, Dict, DefaultDict
 
 
 def get_log_templates(clusters: List) -> Dict:
@@ -8,32 +9,43 @@ def get_log_templates(clusters: List) -> Dict:
     return ret
 
 
-def get_log_structure(log_lines: List, cluster_ids: List, clusters: List) -> List:
+def get_log_structure(log_lines: DefaultDict, cluster_ids: DefaultDict, clusters: List) -> Dict:
     templates = get_log_templates(clusters)
 
-    ret_log_structure = []
-    for curr_id, log in zip(cluster_ids, log_lines):
-        ret_log_structure.append([curr_id, log, templates[curr_id]])
+    ret_log_structure = {key: [] for key in log_lines.keys()}
+    for key in ret_log_structure.keys():
+        for curr_id, log in zip(cluster_ids[key], log_lines[key]):
+            ret_log_structure[key].append((curr_id, log, templates[curr_id]))
     return ret_log_structure
 
 
-def parse_file_drain3(file_path: str):
+def parse_file_drain3(data: DefaultDict) -> Dict:
     template_miner = TemplateMiner()
 
-    cluster_ids = []
-    log_lines = []
-    with open(file_path) as f:
-        for line in f:
-            line = line.rstrip().partition(': ')[2]  # produces tuple (pre, delimiter, post)
+    cluster_ids = defaultdict(list)
+    log_lines = defaultdict(list)
+    for block_id, logs in data.items():
+        for log in logs:
+            line = log.rstrip().partition(': ')[2]  # produces tuple (pre, delimiter, post)
             result = template_miner.add_log_message(line)
-            cluster_ids.append(result['cluster_id'])
-            log_lines.append(line)
+            cluster_ids[block_id].append(result['cluster_id'])
+            log_lines[block_id].append(line)
 
-    ret_log_structure = get_log_structure(log_lines, cluster_ids, template_miner.drain.clusters)
-    return ret_log_structure
+    log_structure = get_log_structure(log_lines, cluster_ids, template_miner.drain.clusters)
+    return log_structure
+
+
+def save_drain3_to_file(data: Dict, file_path: str):
+    with open(file_path, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def load_drain3(file_path: str) -> Dict:
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
 
 
 if __name__ == '__main__':
-    out = parse_file_drain3('../../data/raw/HDFS1/HDFS.log')
-    pd.DataFrame(out, columns=["EventId", "Content", "EventTemplate"]).to_csv(
-        '~/bdip25/src/data' + "/HDFS_Drain3.log_structured-new.csv", index=False)
+    from src.data.hdfs import load_data
+
+    q = parse_file_drain3(load_data('../../data/raw/HDFS1/HDFS.log'))

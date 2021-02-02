337d9d1141535b0e56b849a3a4c174ae24c339de
Martin Korytak <cbudrud@gmail.com>
2021-02-02 15:01:43 +0100

    removed causality

--- src/models/tcnn.py
+++ src/models/tcnn.py
@@ -19,36 +19,23 @@ np.random.seed(SEED)
 torch.manual_seed(SEED)
 
 
-# class Chomp1d(nn.Module):
-#     def __init__(self, chomp_size):
-#         super(Chomp1d, self).__init__()
-#         self.chomp_size = chomp_size
-#
-#     def forward(self, x):
-#         return x[:, :, :-self.chomp_size].contiguous()
-
-
 class TemporalBlock(nn.Module):
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
         super(TemporalBlock, self).__init__()
         self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
                                            stride=stride, padding=padding, dilation=dilation))
-        # self.chomp1 = Chomp1d(padding)
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
 
         self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
                                            stride=stride, padding=padding, dilation=dilation))
-        # self.chomp2 = Chomp1d(padding)
         self.relu2 = nn.ReLU()
         self.dropout2 = nn.Dropout(dropout)
 
         self.net = nn.Sequential(self.conv1,
-                                 # self.chomp1,
                                  self.relu1,
                                  self.dropout1,
                                  self.conv2,
-                                 # self.chomp2,
                                  self.relu2,
                                  self.dropout2)
         self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
@@ -77,7 +64,7 @@ class TemporalConvNet(nn.Module):
             in_channels = num_inputs if i == 0 else num_channels[i - 1]
             out_channels = num_channels[i]
             layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
-                                     padding=(kernel_size - 1) * dilation_size, dropout=dropout)]
+                                     padding=((kernel_size - 1) * dilation_size) // 2, dropout=dropout)]
 
         self.network = nn.Sequential(*layers)
 

904ee48eb01e60ef3acc085e0410cdd1c3f9141d
Martin Korytak <cbudrud@gmail.com>
2021-02-15 12:13:40 +0100

    added script for training AE

---
+++ scripts/train_autoencoder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=4
+#SBATCH --mem=50G
+#SBATCH --gres=gpu:1
+#SBATCH --error=../../logs/autoencoderHDFS1.%j.err
+#SBATCH --out=../../logs/autoencoderHDFS1.%j.out
+
+# clear the environment from any previously loaded modules
+ml purge > /dev/null 2>&1
+
+# add the project to Python's path
+export PYTHONPATH=$PYTHONPATH:/home/korytmar/methods4logfiles
+
+# train TCN model and evaluate hyperparameters on random search
+cd /home/korytmar/methods4logfiles/src/models
+python train_autoencoder.py

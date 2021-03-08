4afd6ed1d42b22c35037d2b8684a1d5459c6b1a9
Martin Korytak <cbudrud@gmail.com>
2021-03-08 12:56:32 +0100

    added transformer with training script

---
+++ src/models/train_transformer.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import numpy as np
+from typing import List, Dict
+
+from src.models.metrics import metrics_report, get_metrics
+from src.models.transformer import TransformerAutoEncoder
+from src.models.train_conv_models import CustomMinMaxScaler
+from src.models.utils import load_pickle_file, find_optimal_threshold, convert_predictions, create_checkpoint, \
+    create_experiment_report, save_experiment
+
+SEED = 160121
+np.random.seed(SEED)
+
+EXPERIMENT_PATH = '../../models/TAE-hyperparameters-embeddings-HDFS1.json'
+
+
+def get_all_divisors(input_dim: int) -> List:
+    # return sorted array of all divisors of `input_dim` in O(2 * sqrt(n)) time
+    # a naive solution would take 0(n + n * log(n)) time
+    divisors = []
+    for i in range(1, int(np.sqrt(input_dim))):
+        if input_dim % i == 0:
+            divisors.append(i)
+    for i in range(int(np.sqrt(input_dim)), 0, -1):
+        if input_dim % i == 0:
+            divisors.append(input_dim // i)
+    return divisors
+
+
+def get_number_of_items_within_range(divisors: List, lower: int, upper: int) -> int:
+    ret = 0
+    for i in divisors:
+        if is_val_in_range(i, lower, upper):
+            ret += 1
+    return ret
+
+
+def is_val_in_range(val: int, lower: int, upper: int) -> bool:
+    return lower <= val <= upper
+
+
+def get_dist(divisors: List) -> List:
+    n_in_range = get_number_of_items_within_range(divisors, 2, 20)
+    n_total = len(divisors)
+
+    norm_dist = 1 / n_total
+    higher_prob_budget = 1.1 * norm_dist * n_in_range
+    lower_prob_budget = 1 - higher_prob_budget
+
+    dist = []
+    for i in divisors:
+        if is_val_in_range(i, 2, 20):
+            dist.append(higher_prob_budget / n_in_range)
+        else:
+            dist.append(lower_prob_budget / (n_total - n_in_range))
+    return dist
+
+
+def train_transformer(x_train: List, x_test: List, y_train: np.array, y_test: np.array) -> Dict:
+    sc = CustomMinMaxScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+
+    model = TransformerAutoEncoder()
+    n_experiments = 100
+    embeddings_dim = x_train[0].shape[1]
+
+    divisors = get_all_divisors(embeddings_dim)
+    params = {
+        'epochs': np.random.choice(np.arange(1, 10), size=n_experiments).tolist(),
+        'learning_rate': np.random.choice(10 ** np.linspace(-4, -0.5), size=n_experiments).tolist(),
+        'batch_size': np.random.choice([2 ** i for i in range(3, 8)], size=n_experiments).tolist(),
+        'input_dim': [embeddings_dim] * n_experiments,
+        'heads': np.random.choice(divisors, size=n_experiments, p=get_dist(divisors)).tolist(),
+        'n_encoders': np.random.randint(1, 10, size=n_experiments).tolist(),
+        'n_decoders': np.random.randint(1, 10, size=n_experiments).tolist(),
+        'dim_feedforward': np.random.randint(50, 500, size=n_experiments).tolist(),
+        'window': np.random.randint(10, 100, size=n_experiments).tolist(),
+        'dropout': np.random.uniform(0, 0.5, size=n_experiments).tolist()
+    }
+    evaluated_hyperparams = random_search((x_train[y_train == 0], x_test, None, y_test), model, params)
+    return evaluated_hyperparams
+
+
+def random_search(data_and_labels: tuple, model: TransformerAutoEncoder, params: Dict) -> Dict:
+    x_train, x_test, _, y_test = data_and_labels
+
+    scores = []
+    for conf in zip(*params.values()):
+        kwargs = {k: val for k, val in zip(params.keys(), conf)}
+
+        model.set_params(**kwargs)
+
+        print(f'Model current hyperparameters are: {kwargs}.')
+
+        model.fit(x_train)
+        y_pred = model.predict(x_test)  # return reconstruction errors
+
+        theta, f1 = find_optimal_threshold(y_test, y_pred)
+        y_pred = convert_predictions(y_pred, theta)
+        metrics_report(y_test, y_pred)
+        scores.append(create_experiment_report(get_metrics(y_test, y_pred), kwargs))
+        # visualize_distribution_with_labels(y_pred, y_test, to_file=False)
+        from sklearn.metrics import confusion_matrix
+        print(confusion_matrix(y_test, y_pred))
+        create_checkpoint({'experiments': scores}, EXPERIMENT_PATH)
+    return {
+        'experiments': scores
+    }
+
+
+if __name__ == '__main__':
+    X_train = load_pickle_file('../../data/processed/HDFS1/X-train-HDFS1-cv1-1-block.pickle')
+    X_val = load_pickle_file('../../data/processed/HDFS1/X-val-HDFS1-cv1-1-block.pickle')
+    y_train = np.load('../../data/processed/HDFS1/y-train-HDFS1-cv1-1-block.npy')
+    y_val = np.load('../../data/processed/HDFS1/y-val-HDFS1-cv1-1-block.npy')
+
+    # X_val = load_pickle_file('../../data/processed/HDFS1/X-val-HDFS1-cv1-1-block.npy')
+    # y_val = np.load('../../data/processed/HDFS1/y-val-HDFS1-cv1-1-block.npy')
+
+    # results = train_autoencoder(X_val[:1000], X_val[:500], y_val[:1000], y_val[:500])
+    results = train_transformer(X_train, X_val, y_train, y_val)
+    save_experiment(results, EXPERIMENT_PATH)
---
+++ src/models/transformer.py
@@ -0,0 +1,144 @@
+from __future__ import annotations
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import numpy as np
+import sklearn
+from tqdm import tqdm
+from typing import List
+import sys
+
+from src.models.utils import time_decorator
+from src.models.datasets import EmbeddingDataset, CroppedDataset1D
+
+SEED = 160121
+
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+
+
+class TransformerAutoEncoder(sklearn.base.OutlierMixin):
+    def __init__(self, epochs: int = 1, batch_size: int = 32, optimizer: str = 'adam',
+                 loss: str = 'mean_squared_error', learning_rate: float = 0.001,
+                 dataset_type: str = 'cropped', window: int = 35, verbose: int = True):
+        # add dictionary with architecture of the model i.e., number of layers, hidden units per layer etc.
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.optimizer = optimizer
+        self.loss = loss
+        self.learning_rate = learning_rate
+        self.dataset_type = dataset_type
+        self.window = window
+        self.verbose = verbose
+
+        # internal representation of a torch model
+        self._model = None
+        self._device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    def fit(self, X: np.ndarray) -> TransformerAutoEncoder:
+        # 1. convert to torch Tensor
+        train_dl = self._numpy_to_tensors(X, batch_size=self.batch_size, shuffle=True)
+
+        # 2. initialize model
+        if not self._model:
+            self._initialize_model(X[0].shape[-1], 1, 1, 1, 256, 0.2)
+
+        loss_function = self._get_loss_function()
+        opt = self._get_optimizer()
+
+        for epoch in range(self.epochs):
+            self._model.train()
+            loss, execution_time = self._train_epoch(train_dl, opt, loss_function)
+
+            if self.verbose:
+                digits = int(np.log10(self.epochs)) + 1
+                print(f'Epoch: {epoch + 1:{digits}}/{self.epochs}, loss: {loss:.5f}, time: {execution_time:.5f} s')
+        del train_dl  # free the memory
+        return self
+
+    def predict(self, X: np.ndarray) -> np.array:
+        test_dl = self._numpy_to_tensors(X, batch_size=64, shuffle=False)
+
+        loss_function = self._get_loss_function(reduction='none')
+
+        self._model.eval()
+        with torch.no_grad():
+            ret = []
+            for (batch,) in test_dl:
+                batch = batch.to(self._device)
+                batch = batch.permute(2, 0, 1)
+
+                ret.extend(torch.mean(loss_function(self._model(batch, batch), batch), (0, 2)).tolist())
+            del test_dl  # free the memory
+            return np.asarray(ret)
+
+    def set_params(self, **kwargs):
+        self.epochs = kwargs['epochs']
+        self.batch_size = kwargs['batch_size']
+        self.learning_rate = kwargs['learning_rate']
+        self.window = kwargs['window']
+        self._initialize_model(kwargs['input_dim'], kwargs['heads'], kwargs['n_encoders'], kwargs['n_decoders'],
+                               kwargs['dim_feedforward'], kwargs['dropout'])
+
+    def _initialize_model(self, input_dim: int, n_heads: int, n_encoders: int, n_decoders: int, fc_dim: int,
+                          dropout: float):
+        self._model = nn.Transformer(input_dim, n_heads, n_encoders, n_decoders, fc_dim, dropout)
+        self._model.to(self._device)
+
+    def _get_loss_function(self, reduction: str = 'mean') -> nn.Module:
+        if self.loss == 'mean_squared_error':
+            return nn.MSELoss(reduction=reduction)
+        elif self.loss == 'kullback_leibler_divergence':
+            return nn.KLDivLoss(reduction=reduction)
+        else:
+            raise NotImplementedError(f'"{self.loss}" is not implemented.')
+
+    def _get_optimizer(self) -> torch.optim.Optimizer:
+        if self.optimizer == 'adam':
+            return torch.optim.Adam(self._model.parameters(), lr=self.learning_rate)
+        else:
+            raise NotImplementedError(f'"{self.optimizer}" is not implemented.')
+
+    @staticmethod
+    def custom_collate(data: List) -> List:
+        # randomly shuffle data within a batch
+        tensor = data[0]
+        indexes = torch.randperm(tensor.shape[0])
+        return [tensor[indexes]]  # must stay persistent with PyTorch API
+
+    def _numpy_to_tensors(self, X: np.ndarray, batch_size: int, shuffle: bool) -> DataLoader:
+        if self.dataset_type == 'variable_sized':
+            train_ds = EmbeddingDataset(X, batch_size=batch_size)
+            collate_fn = self.custom_collate if shuffle else None
+            train_dl = DataLoader(train_ds, batch_size=1, shuffle=shuffle, collate_fn=collate_fn)
+        elif self.dataset_type == 'cropped':
+            train_ds = CroppedDataset1D(X, window=self.window)
+            train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=shuffle)
+        else:
+            raise NotImplementedError('This dataset preprocessing is not implemented yet.')
+        del train_ds  # free the memory
+        return train_dl
+
+    @time_decorator
+    def _train_epoch(self, train_dl: DataLoader, optimizer: torch.optim.Optimizer, criterion: nn.Module) -> float:
+        loss = 0
+        n_seen_examples = 0
+        train_dl = tqdm(train_dl, file=sys.stdout, ascii=True, unit='batch')
+        for (batch,) in train_dl:
+            batch = batch.to(self._device)
+            batch = batch.permute(2, 0, 1)
+
+            optimizer.zero_grad()
+
+            pred = self._model(batch, batch)
+            batch_loss = criterion(pred, batch)
+
+            batch_loss.backward()
+            optimizer.step()
+
+            loss += batch_loss.item() * batch.size(0)
+            n_seen_examples += batch.size(0)
+
+            train_dl.set_postfix({'loss': loss / n_seen_examples, 'curr_loss': batch_loss.item()})
+        return loss / n_seen_examples

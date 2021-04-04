a512d92c6c3c3846b0ea943976b0a85c260d582f
Martin Korytak <cbudrud@gmail.com>
2021-04-04 20:29:34 +0200

    additional set of random seed

--- src/experiments/replicate_results.py
+++ src/experiments/replicate_results.py
@@ -24,6 +24,7 @@ from src.models.utils import create_experiment_report, create_checkpoint, save_e
 
 SEED = 160121
 np.random.seed(SEED)
+torch.manual_seed(SEED)
 
 DIR_TO_EXPERIMENTS = '../../models/aecnn1d'
 EXPERIMENT_PATH = os.path.join(DIR_TO_EXPERIMENTS, 'experiments.json')

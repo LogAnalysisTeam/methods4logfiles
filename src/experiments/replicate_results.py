9e1052a0cb24d8712e953c2a97009bcc38545f8d
Martin Korytak <cbudrud@gmail.com>
2021-04-09 12:01:46 +0200

    added baseline AE

--- src/experiments/replicate_results.py
+++ src/experiments/replicate_results.py
@@ -8,6 +8,8 @@ from typing import List, Dict, Union
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
+from src.data.hdfs import load_labels
+from src.features.feature_extractor import FeatureExtractor
 from src.models.autoencoder import AutoEncoder
 from src.models.vanilla_tcnn import VanillaTCN
 from src.models.autoencoder_tcnn import AETCN
@@ -19,6 +21,7 @@ from src.models.sa_cnn1d import SACNN1D
 from src.models.sa_cnn2d import SACNN2D
 from src.models.train_hybrid_model import get_extracted_features
 from src.models.metrics import metrics_report, get_metrics
+from src.models.train_baseline_models import get_labels_from_csv
 from src.models.utils import create_experiment_report, create_checkpoint, save_experiment, load_pickle_file, \
     find_optimal_threshold, classify, load_experiment, create_model_path, convert_predictions
 
@@ -26,7 +29,7 @@ SEED = 160121
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-DIR_TO_EXPERIMENTS = '../../models/aecnn1d'
+DIR_TO_EXPERIMENTS = '../../models/ae_baseline'
 EXPERIMENT_PATH = os.path.join(DIR_TO_EXPERIMENTS, 'experiments.json')
 
 
@@ -170,6 +173,20 @@ def train_hybrid_model_if(x_train: List, x_test: List, y_train: np.array, y_test
     return evaluated_hyperparams
 
 
+def train_autoencoder(x_train: Dict, x_test: Dict, y_train: np.array, y_test: np.array) -> Dict:
+    fe = FeatureExtractor(method='tf-idf', preprocessing='mean')
+    y_train = get_labels_from_csv(y_train, x_train.keys())
+    y_test = get_labels_from_csv(y_test, x_test.keys())
+    x_train = fe.fit_transform(x_train)
+    x_test = fe.transform(x_test)
+
+    model = AutoEncoder()
+
+    experiments = load_experiment('../../models/AE-hyperparameters-Drain3-HDFS1.json')
+    evaluated_hyperparams = random_search((x_train[y_train == 0], x_test, None, y_test), model, experiments)
+    return evaluated_hyperparams
+
+
 def random_search(data_and_labels: tuple, model: Union[AutoEncoder, VanillaTCN, AETCN, AECNN1D, CNN1D, CNN2D, TCNCNN1D,
                                                        SACNN1D, SACNN2D], params: Dict) -> Dict:
     x_train, x_test, _, y_test = data_and_labels
@@ -225,13 +242,6 @@ def random_search_unsupervised(data_and_labels: tuple, model: IsolationForest, p
 
 
 if __name__ == '__main__':
-    debug = False
-    if debug:
-        X_val = load_pickle_file('../../data/processed/HDFS1/X-val-HDFS1-cv1-1-block.npy')
-        y_val = np.load('../../data/processed/HDFS1/y-val-HDFS1-cv1-1-block.npy')
-
-        train_tcnn_cnn1d(X_val[:1000], X_val[:500], y_val[:1000], y_val[:500])
-
     X_train = load_pickle_file('../../data/processed/HDFS1/X-train-HDFS1-cv1-1-block.pickle')
     X_val = load_pickle_file('../../data/processed/HDFS1/X-val-HDFS1-cv1-1-block.pickle')
     y_train = np.load('../../data/processed/HDFS1/y-train-HDFS1-cv1-1-block.npy')
@@ -252,8 +262,8 @@ if __name__ == '__main__':
     # results = train_aetcnn(X_train, X_val, y_train, y_val)
     # save_experiment(results, EXPERIMENT_PATH)
 
-    results = train_aecnn1d(X_train, X_val, y_train, y_val)
-    save_experiment(results, EXPERIMENT_PATH)
+    # results = train_aecnn1d(X_train, X_val, y_train, y_val)
+    # save_experiment(results, EXPERIMENT_PATH)
 
     # results = train_sa_cnn1d(X_train, X_val, y_train, y_val)
     # save_experiment(results, EXPERIMENT_PATH)
@@ -278,3 +288,13 @@ if __name__ == '__main__':
     #
     # results = train_hybrid_model_if(X_train, X_val, y_train, y_val)
     # save_experiment(results, EXPERIMENT_PATH)
+
+    ########################### BASELINE METHODS #######################################################################
+
+    X_train = load_pickle_file('../../data/interim/HDFS1/train-data-Drain3-HDFS1-cv1-1.binlog')
+    y_train = load_labels('../../data/interim/HDFS1/train-labels-HDFS1-cv1-1.csv')
+    X_val = load_pickle_file('../../data/interim/HDFS1/val-data-Drain3-HDFS1-cv1-1.binlog')
+    y_val = load_labels('../../data/interim/HDFS1/val-labels-HDFS1-cv1-1.csv')
+
+    results = train_autoencoder(X_train, X_val, y_train, y_val)
+    save_experiment(results, EXPERIMENT_PATH)
